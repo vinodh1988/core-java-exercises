20360edeb351afaf1e7f04f265805fd58374b614
vinodh1988 <k.c.vinodh@hotmail.com>
2022-03-12 10:26:47 +0530

    project-commit

---
+++ Demo/src/com/sterling/samples/InstanceDemo.java
@@ -0,0 +1,23 @@
+package com.sterling.samples;
+
+/*
+ * 
+ *  Java has four access specifiers
+ *  
+ *      private
+ *      protected
+ *      public
+ *      default - not  a keyword, if nothing specified taken as default
+ *      
+ *    outer classes in java can have only public and default access specifiers
+ *    
+ *    inner classes can have any access specifier
+ *    
+ *    in a file, only one public class is allowed
+ *    
+ *    it a rule that the name of the file should be same as that of the public class
+ */
+public class InstanceDemo {
+
+}
+
