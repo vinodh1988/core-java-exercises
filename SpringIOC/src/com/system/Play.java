e441f623d4a31597e2e54c68c28dfc23f76fa5b5
vinodhleo <vinodhleo@outlook.com>
2022-03-26 10:51:03 +0530

    system-commit

---
+++ SpringIOC/src/com/system/Play.java
@@ -0,0 +1,11 @@
+package com.system;
+
+public class Play implements Task{
+
+	@Override
+	public void perform() {
+		// TODO Auto-generated method stub
+		System.out.println("Playing.....");
+	}
+
+}
