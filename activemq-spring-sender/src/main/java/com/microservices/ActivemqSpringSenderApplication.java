ca7e53c4b36318aadf5375e85afbd5d3a6496c6e
vinodh1988 <k.c.vinodh@hotmail.com>
2022-04-23 09:40:20 +0530

    active-mq-commit

---
+++ activemq-spring-sender/src/main/java/com/microservices/ActivemqSpringSenderApplication.java
@@ -0,0 +1,29 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+
+@SpringBootApplication
+public class ActivemqSpringSenderApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ac=SpringApplication.run(ActivemqSpringSenderApplication.class, args);
+		JmsTemplate  jms=ac.getBean(JmsTemplate.class);
+		while(true) {
+			long id=Math.round(Math.random()*10000);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(id==9495)
+				break;
+			jms.convertAndSend("FirstQueue","Message "+id);
+		}
+		
+	}
+
+}
